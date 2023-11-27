43ae6a577b72bf5b97c3c264b179479e1fd15de1
surikatho <nombre>
2023-11-27 16:57:19 +0100

    e añadido el boostrap e importado el home al main que se me habia olvidado .

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import {Home} from './componentes/Home'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home></Home>
