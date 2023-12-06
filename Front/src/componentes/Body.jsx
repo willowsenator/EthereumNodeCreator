1afe50d9a4683a71573a3c8ddb30294c1d64ca97
surikatho <nombre>
2023-12-06 22:20:20 +0100

    e modificado visualmente el componente Transferencia, añadido bostrap y react-boostrap,el pg,modificado el body para que sea el nodecreator, e dejado el home como home y e añadido estilos al link en css

--- Front/src/componentes/Body.jsx
+++ Front/src/componentes/Body.jsx
@@ -2,32 +2,52 @@ import React, { useState } from 'react';
 import './styles.css'; 
 
 function Body() {
-  const [networkId, setNetworkId] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [numeroRed, setNumeroRed] = useState('');
+  const [numeroNodos, setNumeroNodos] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Número de Red:', networkId);
+    console.log('Nombre:', nombre, 'Número de Red:', numeroRed, 'Número de Nodos:', numeroNodos);
+    // agregar lógica para interactuar con una API o base de datos
   };
 
   return (
-    <div className="body-container">
-      <div className="network-form-container">
-        <form onSubmit={handleSubmit} className="network-form">
-          <div className="form-group">
-            <label htmlFor="networkId">Número de Nodos:</label>
-            <input
-              type="text"
-              id="networkId"
-              value={networkId}
-              onChange={(e) => setNetworkId(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="submit-btn">Crear Red</button>
-        </form>
+      <div className="body-container">
+        <div className="network-form-container">
+          <form onSubmit={handleSubmit} className="network-form">
+            <div className="form-group">
+              <label htmlFor="nombre">Nombre:</label>
+              <input
+                type="text"
+                id="nombre"
+                // ... tus otros atributos ...
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="numeroRed">Número de Red:</label>
+              <input
+                type="text"
+                id="numeroRed"
+                // ... tus otros atributos ...
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="numeroNodos">Número de Nodos:</label>
+              <input
+                type="text"
+                id="numeroNodos"
+                // ... tus otros atributos ...
+              />
+            </div>
+            <button type="submit" className="submit-btn">Crear Red</button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
+
 
 export default Body;
+
--- Front/src/componentes/Transferencia.jsx
+++ Front/src/componentes/Transferencia.jsx
@@ -1,53 +1,96 @@
 import React, { useState } from 'react';
 import Web3 from 'web3';
 import { Link } from 'react-router-dom';
+import { Button, Form, Alert, Spinner, Row, Col } from 'react-bootstrap';
 
 function Transferencia() {
     const [recipient, setRecipient] = useState('');
     const [amount, setAmount] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [feedback, setFeedback] = useState({ type: '', message: '' }); // Estado para el feedback
 
     const sendTransaction = async () => {
+        // Antes de intentar enviar, verifica que hay un destinatario y una cantidad
+        if (!recipient || !amount) {
+        setFeedback({ type: 'error', message: 'Por favor, llena todos los campos.' });
+        return;
+        }
+        
+        // Validación de la dirección del destinatario
         if (!Web3.utils.isAddress(recipient)) {
-            alert('La dirección del destinatario no es válida.');
-            return;
+        setFeedback({ type: 'error', message: 'La dirección del destinatario no es válida.' });
+        return;
         }
 
+        // Configura el estado de carga y limpia mensajes anteriores
+        setLoading(true);
+        setFeedback({ type: '', message: '' });
+
+        // Configura tu instancia de Web3, reemplaza "tu-url-rpc" con tu URL de RPC
         const web3 = new Web3(Web3.givenProvider || "tu-url-rpc");
         try {
-            const accounts = await web3.eth.getAccounts();
-            const transactionParameters = {
-                to: recipient,
-                from: accounts[0],
-                value: web3.utils.toHex(web3.utils.toWei(amount, 'ether'))
-            };
+        // Obtiene las cuentas y envía la transacción
+        const accounts = await web3.eth.getAccounts();
+        const transactionParameters = {
+            to: recipient,
+            from: accounts[0],
+            value: web3.utils.toHex(web3.utils.toWei(amount, 'ether')),
+        };
 
-            await web3.eth.sendTransaction(transactionParameters);
-            alert('Transacción enviada');
+        await web3.eth.sendTransaction(transactionParameters);
+        setFeedback({ type: 'success', message: 'Transacción enviada con éxito.' });
         } catch (error) {
-            alert(`Error al enviar la transacción: ${error.message}`);
+        setFeedback({ type: 'error', message: `Error al enviar la transacción: ${error.message}` });
+        } finally {
+        setLoading(false);
         }
     };
 
     return (
-        <div>
-            <input
-                type="text"
-                value={recipient}
-                onChange={(e) => setRecipient(e.target.value)}
-                placeholder="Dirección del destinatario"
-            />
-            <input
-                type="text"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                placeholder="Cantidad a enviar"
-            />
-            <button onClick={sendTransaction}>Enviar Ether</button>
-            <div>
-                <Link to="/">Volver al Inicio</Link>
-            </div>
+        <div className="transferencia-container">
+        {feedback.message && (
+            <Alert variant={feedback.type === 'error' ? 'danger' : 'success'}>
+            {feedback.message}
+            </Alert>
+        )}
+        <Form>
+            <Row className="align-items-end">
+            <Col xs={12} md={7}>
+                <Form.Group>
+                <Form.Label>Dirección del destinatario</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={recipient}
+                    onChange={(e) => setRecipient(e.target.value)}
+                    placeholder="Ingresa la dirección del destinatario"
+                    isInvalid={feedback.type === 'error'}
+                />
+                </Form.Group>
+            </Col>
+            <Col xs={12} md={3}>
+                <Form.Group>
+                <Form.Label>Cantidad</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    placeholder="Ingresa la cantidad a enviar"
+                />
+                </Form.Group>
+            </Col>
+            <Col xs={12} md={2}>
+                <Button variant="primary" onClick={sendTransaction} disabled={loading}>
+                {loading ? (
+                    <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+                ) : (
+                    'Enviar Ether'
+                )}
+                </Button>
+            </Col>
+            </Row>
+        </Form>
+        <Link to="/" className="back-link">Volver al Inicio</Link>
         </div>
     );
-}
-
+    }
 export default Transferencia;
