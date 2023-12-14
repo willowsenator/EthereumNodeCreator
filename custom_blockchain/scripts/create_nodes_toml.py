0d5551b92333afa5c04a53bf022d708cabf539ca
willowsenator <willowsenator@gmail.com>
2023-12-14 01:28:01 +0100

    T013-Añadir puertos rpc a los toml

--- custom_blockchain/scripts/create_nodes_toml.py
+++ custom_blockchain/scripts/create_nodes_toml.py
@@ -26,6 +26,10 @@ toml_template = "node_template.toml"
 # Parameters for miner
 etherbase = "Etherbase="
 
+# NodeTypes
+miner = "miner"
+rpc = "rpc"
+
 
 def create_conf_directories(args):
     path = os.path.join(base_directory, networks_directory, args.network_id, conf_directory)
@@ -48,7 +52,7 @@ def create_rpcs_toml(args):
                                  rpc_nodes_directory)
     rpc_nodes_path = os.path.join(base_directory, networks_directory, args.network_id, rpc_nodes_directory)
     rpc_node_names = os.listdir(rpc_nodes_path)
-    create_current_toml(args, rpc_node_names, rpc_toml_path)
+    create_current_toml(args, rpc_node_names, rpc_toml_path, rpc)
 
 
 def create_miners_toml(args):
@@ -56,7 +60,7 @@ def create_miners_toml(args):
                                    miner_nodes_directory)
     miner_nodes_path = os.path.join(base_directory, networks_directory, args.network_id, miner_nodes_directory)
     miner_node_names = os.listdir(miner_nodes_path)
-    create_current_toml(args, miner_node_names, miner_toml_path, "miner")
+    create_current_toml(args, miner_node_names, miner_toml_path, miner)
 
 
 def create_common_nodes_toml(args):
@@ -81,18 +85,25 @@ def create_current_toml(args, node_names, toml_path, node_type=""):
         print(f"Creado toml {current_node_toml_path}")
 
 
-def modified_toml_template(args, content, node_type, item):
+def modified_toml_template(args, content, node_type, node_name):
     modified_content = content.replace("%NETWORK_ID%", args.network_id)
     if node_type == "miner":
         current_miner_path = os.path.join(base_directory, networks_directory, args.network_id, miner_nodes_directory
-                                          , item, keystore_directory)
+                                          , node_name, keystore_directory)
         current_miner_keystore = os.path.join(current_miner_path, os.listdir(current_miner_path)[0])
         modified_content = modified_content.replace("%ETHER_BASE%",
                                                     f'{etherbase}"{get_node_account(current_miner_keystore)}"')
     else:
         modified_content = modified_content.replace("%ETHER_BASE%", "")
 
-    modified_content = modified_content.replace("%NODE_NAME%", item)
+    modified_content = modified_content.replace("%NODE_NAME%", node_name)
+
+    if node_type == "rpc":
+        pattern_index = node_name[len(rpc_node_name):]
+        rpc_ports = get_rpc_ports(args)
+        modified_content = modified_content.replace("%HTTP_PORT%", f"{rpc_ports[int(pattern_index)]}")
+    else:
+        modified_content = modified_content.replace("%HTTP_PORT%", "0")
 
     return modified_content
 
@@ -124,14 +135,39 @@ def create_common_node_conf_directory(args):
     print(f"Creado {common_node_conf_path}")
 
 
+def validate(args):
+    validate_num_rpc_ports(args)
+
+
+def get_rpc_ports(args):
+    rpc_ports = []
+    if ',' in args.rpc_ports:
+        for item in args.rpc_ports.split(','):
+            rpc_ports.append(item)
+    else:
+        rpc_ports.append(args.rpc_ports)
+    return rpc_ports
+
+
+def validate_num_rpc_ports(args):
+    rpc_nodes_path = os.path.join(base_directory, networks_directory, args.network_id, rpc_nodes_directory)
+    rpc_nodes = os.listdir(rpc_nodes_path)
+    rpc_ports = get_rpc_ports(args)
+
+    if not len(rpc_nodes) == len(rpc_ports):
+        raise ValueError(
+            f"Num de puertos rpc tiene que ser igual al número de nodos rpc. Num nodos rpc: {len(rpc_nodes)}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Generate nodes toml")
     parser.add_argument("-network_id", required=True, help="Network id for the network")
-    # parser.add_argument("-rpc_ports", required=True, help="Rpc ports")
-    # parser.add_argument("-auth_ports", required=True, help="Auth ports")
-    # parser.add_argument("-nodes_ports", required=True, help="Node ports")
+    parser.add_argument("-rpc_ports", required=True, help="Rpc port or several rpc ports separated by ,")
+    # parser.add_argument("-auth_ports", required=True, help="Auth port or several auth ports separated by ,")
+    # parser.add_argument("-nodes_ports", required=True, help="Node ports separated by ,")
     args = parser.parse_args()
 
+    validate(args)
     create_conf_directories(args)
     create_tomls(args)
 
